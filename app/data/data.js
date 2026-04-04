1351c9b5c4ea83108957736b87db8936b0d7894e
Godswill-joel <ge42217@gmail.com>
2026-04-04 12:34:55 +0100

    fix: remove Blog link from navigation menu

--- app/data/data.js
+++ app/data/data.js
@@ -10,7 +10,6 @@ export const navLinks = [
   { name: "Home", href: "/" },
   { name: "About", href: "/about" },
   {name: "Services", href: "/services",},
-  { name: "Blog", href: "/blog" },
   { name: 'Contact Us', href: '/contact' },
 ];
 
